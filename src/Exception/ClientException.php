9ce05ea895d90209d4196cc7a6ca871125dbaa32
lzy <fuzzyy@foxmail.com>
2020-01-07 11:45:42 +0800

    init

---
+++ src/Exception/ClientException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JavaReact\FuluOpenApi\Exception;
+
+
+use RuntimeException;
+
+class ClientException extends RuntimeException implements FuluOpenApiException
+{
+
+}
---
+++ src/Exception/ServerException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace JavaReact\FuluOpenApi\Exception;
+
+
+use RuntimeException;
+
+class ServerException extends RuntimeException implements FuluOpenApiException
+{
+
+}
